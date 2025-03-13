a9be567a61f36344b49593d54c08e0b24fe2a389
Simona Trajkovska <137436120+Simonatraj@users.noreply.github.com>
2025-03-13 15:22:50 +0100

    Update app_skill_matcher.py

--- app_skill_matcher.py
+++ app_skill_matcher.py
@@ -15,7 +15,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password=##,
+        password='',
         database="skill_matcher"
     )
 
